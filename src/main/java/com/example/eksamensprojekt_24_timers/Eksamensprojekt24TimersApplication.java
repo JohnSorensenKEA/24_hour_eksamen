9baa86e5c320dc4b1e560a86bfb2bd06997fd0e1
John <john383p@stud.kea.dk>
2021-06-10 10:04:22 +0200

    first commit

---
+++ src/main/java/com/example/eksamensprojekt_24_timers/Eksamensprojekt24TimersApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojekt_24_timers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamensprojekt24TimersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamensprojekt24TimersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamensprojekt_24_timers/Eksamensprojekt24TimersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojekt_24_timers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamensprojekt24TimersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
